f284feb1059643cffc74bc1bbd32be38aaa9ce31
Michal Vaniš <zirixcz@gmail.com>
2024-03-03 16:24:10 +0100

    store api keys in data & add support for ollama

--- lua/nvimai.lua
+++ lua/nvimai.lua
@@ -3,10 +3,29 @@ local M = {}
 local buffer = nil
 local window = nil
 
+---@class OpenAIConfig
+---@field max_tokens number The maximum number of tokens to generate.
+---@field model string The model to use for generation.
+
+---@class OllamaConfig
+---@field model string The model to use for queries.
+
+---@class NvimAIConfig
+---@field integration string The integration to use, can be 'openai' or 'ollama'.
+---@field openai OpenAIConfig Configuration settings for OpenAI.
+---@field ollama OllamaConfig Configuration settings for Ollama.
+
+---Configurations for Nvim AI setup
+---@type NvimAIConfig
 local config = {
-	api_key = "",
-	max_tokens = 1000,
-	model = "gpt-3.5-turbo-0125",
+	integration = "openai",
+	openai = {
+		max_tokens = 1000,
+		model = "gpt-3.5-turbo-0125",
+	},
+	ollama = {
+		model = "llama2",
+	},
 }
 
 function M.setup(options)
@@ -25,8 +44,24 @@ function M.setup(options)
 	return M
 end
 
+function M.save_api_key_to_file(api_key)
+	local path = vim.fn.stdpath("data") .. "/nvimai.txt"
+	vim.fn.writefile({ api_key }, path)
+end
+
+function M.load_api_key_from_file()
+	local path = vim.fn.stdpath("data") .. "/nvimai.txt"
+	if vim.fn.filereadable(path) == 1 then
+		local lines = vim.fn.readfile(path)
+		if #lines > 0 then
+			return lines[1]
+		end
+	end
+	return nil
+end
+
 function M.save_buffer_to_file()
-	local path = vim.fn.stdpath("data") .. "/chatgpt_history.txt"
+	local path = vim.fn.stdpath("data") .. "/nvimai.txt"
 	if buffer and vim.api.nvim_buf_is_valid(buffer) then
 		local lines = vim.api.nvim_buf_get_lines(buffer, 0, -1, false)
 		vim.fn.writefile(lines, path)
@@ -34,7 +69,7 @@ function M.save_buffer_to_file()
 end
 
 function M.load_buffer_from_file()
-	local path = vim.fn.stdpath("data") .. "/chatgpt_history.txt"
+	local path = vim.fn.stdpath("data") .. "/nvimai.txt"
 	if vim.fn.filereadable(path) == 1 then
 		local lines = vim.fn.readfile(path)
 		if not buffer or not vim.api.nvim_buf_is_valid(buffer) then
@@ -59,7 +94,7 @@ function M.extract_language_and_code(content)
 	return language or "text", code or content
 end
 
-function M.open_window(content, filetype, tokens)
+function M.open_window(content, filetype, tokens, model)
 	if buffer == nil or not vim.api.nvim_buf_is_valid(buffer) then
 		buffer = vim.api.nvim_create_buf(true, false)
 	end
@@ -75,7 +110,7 @@ function M.open_window(content, filetype, tokens)
 
 	local timestamp = os.date("%Y-%m-%d %H:%M:%S")
 	local header = "-- " .. timestamp .. " --"
-	local model = "-- " .. config.model .. " --"
+	local model = "-- " .. model .. " --"
 	local token_info = "-- tokens used: " .. tokens .. " --"
 
 	local new_content = vim.split(content, "\n")
@@ -101,35 +136,18 @@ function M.open_window(content, filetype, tokens)
 	M.save_buffer_to_file()
 end
 
-function M.magic(input)
+function M.openai(input)
 	M.load_buffer_from_file()
+	local api_key = M.load_api_key_from_file()
+	local model = config.openai.model
+	local max_tokens = config.openai.max_tokens
 
-	local api_key = config.api_key
-	local model = config.model
-	local max_tokens = config.max_tokens
-
-	if api_key == "" then
+	if type(api_key) ~= "string" then
 		local setupMessage = [[
 No API key found. Please set it up with:
-
-Lazy:
-{
-  "zirixcz/nvimai",
-  opts = {
-    model = "gpt-4-0125-preview",
-    max_tokens = 1000,
-    api_key = "yourapikey",
-  },
-}
-
-Or with require:
-require('nvimai').setup({
-  model = "gpt-4-0125-preview",
-  max_tokens = 1000,
-  api_key = "yourapikey"
-})
+:NAISetup yourapikey
     ]]
-		M.open_window(setupMessage, "", 0)
+		M.open_window(setupMessage, "", 0, config.openai.model)
 		return
 	end
 
@@ -164,12 +182,12 @@ require('nvimai').setup({
 					local tokens = decoded.usage.total_tokens
 					if message then
 						local language, code = M.extract_language_and_code(message)
-						M.open_window(code, language, tokens)
+						M.open_window(code, language, tokens, config.openai.model)
 					else
-						M.open_window("Received an unexpected structure from API.", "", 0)
+						M.open_window("Received an unexpected structure from API.", "", 0, config.openai.model)
 					end
 				else
-					M.open_window("No choices returned from API.", "", 0)
+					M.open_window("No choices returned from API.", "", 0, config.openai.model)
 				end
 			end
 		end,
@@ -182,11 +200,74 @@ require('nvimai').setup({
 				local all_lines = table.concat(data, "")
 				if all_lines ~= "" then
 					local error_message = "Error: " .. all_lines
-					M.open_window(error_message, "", 0)
+					M.open_window(error_message, "", 0, config.openai.model)
 				end
 			end
 		end,
 	})
+end
+
+function M.ollama(input)
+	M.load_buffer_from_file()
+
+	local data = string.format('{"model": "%s", "prompt": "%s"}', config.ollama.model, input)
+	local buffer = {}
+
+	vim.fn.jobstart({
+		"curl",
+		"-s",
+		"-X",
+		"POST",
+		"-H",
+		"Content-Type: application/json",
+		"--data",
+		data,
+		"--connect-timeout",
+		"30",
+		"--max-time",
+		"3600",
+		"http://localhost:11434/api/generate",
+	}, {
+		stdout_buffered = false,
+		on_stdout = function(_, data, _)
+			if data then
+				for _, chunk in ipairs(data) do
+					table.insert(buffer, chunk)
+				end
+			end
+			if #data == 0 or data[#data] == "" then
+				local message = ""
+				for _, line in ipairs(buffer) do
+					if line and line ~= "" then
+						if line:sub(1, 1) == "{" and line:sub(-1) == "}" then
+							local status, decoded_line = pcall(vim.fn.json_decode, line)
+							if status and decoded_line then
+								message = message .. decoded_line.response
+							end
+						end
+					end
+				end
+				M.open_window(message, "", -1, config.ollama.model)
+			end
+		end,
+		on_stderr = function(_, data, _)
+			if data then
+				local all_lines = table.concat(data, "")
+				if all_lines ~= "" then
+					local error_message = "Error: " .. all_lines
+					M.open_window(error_message, "", 0, config.ollama.model)
+				end
+			end
+		end,
+	})
+end
+
+function M.router(input)
+	if config.integration == "openai" then
+		M.openai(input)
+	elseif config.integration == "ollama" then
+		M.ollama(input)
+	end
 end
 
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,3 +1,9 @@
+local nvimai = require("nvimai")
+
 vim.api.nvim_create_user_command("NAI", function(input)
-	require("nvimai").magic(input.args)
+	nvimai.router(input.args)
 end, { nargs = "+" })
+
+vim.api.nvim_create_user_command("NAISetup", function(input)
+	nvimai.save_api_key_to_file(input.args)
+end, { nargs = 1 })
